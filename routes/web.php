fca7a784270c4706694bbb6e6e84a4873b6909f6
Gustavo Ferreira Delfino <gustavo.delfino@kyndryl.com>
2022-05-02 20:44:52 -0300

    Seção 6: Avançando com Rotas (Routes)

--- routes/web.php
+++ routes/web.php
@@ -26,13 +26,51 @@ Route::get('/', function () {
     return 'Olá, seja bem vindo ao curso!';
 });
 */
+//metodo name renomea as rotas para ser utilizado dentro do codigo
+Route::get('/', 'PrincipalController@principal')->name('site.index');
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 
-Route::get('/contato', 'ContatoController@contato');
+Route::get('/login', function(){ return 'Login';})->name('site.login');
 
+// Metodo prefix() para agrupar rotas
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){ return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){ return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'Produtos';})->name('app.produtos');
+});
+
+//Redirecionamento de rotas
+Route::get('/rota1', function() {
+    echo 'Rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function() {
+   return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+//Route::redirect('/rota2', 'rota1');
+
+//O metodo de fallback possibilta com que tratamos o erro de quando user acessa uma rota que não está disponivel.
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
+});
+
+//Recebendo parametro por meio da URL
+//Inserindo o caractere ? o parameto fica opcional
+//rota de teste
+/*Route::get(
+    '/contato/{nome}/{categoria_id}', 
+    function(
+        string $nome = 'Desconhecido', 
+        int $categoria_id = 1 // 1 - 'Informação'
+    ) {
+        echo "Estamos aqui: $nome - $categoria_id";
+    }
+)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+*/
 /* verbo http
 
 get
