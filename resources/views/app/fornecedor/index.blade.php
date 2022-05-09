4f3821790e444bc212b493d5bb8d5d1eaf2c0944
Gustavo Ferreira Delfino <gustavo.delfino@kyndryl.com>
2022-05-08 22:56:13 -0300

    53. Blade - @while

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -16,7 +16,8 @@
 @endphp
 
 @isset($fornecedores)
-    @for($i = 0; isset($fornecedores[$i]); $i++)
+    @php $i = 0 @endphp
+    @while(isset($fornecedores[$i]))
         Fornecedor: {{ $fornecedores[$i]['nome'] }}
         <br>
         Status: {{ $fornecedores[$i]['status'] }}
@@ -25,5 +26,6 @@
         <br>
         Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }}) {{ $fornecedores[$i]['telefone'] ?? '' }}
         <hr>
-    @endfor
+        @php $i++ @endphp
+    @endwhile
 @endisset
