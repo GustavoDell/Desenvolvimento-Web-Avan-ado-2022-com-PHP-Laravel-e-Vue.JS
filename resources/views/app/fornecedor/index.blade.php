018869c5c7fe7fc3df189e908ceafd0e0ad0468a
Gustavo Ferreira Delfino <gustavo.delfino@kyndryl.com>
2022-05-08 22:39:08 -0300

    51. Blade - @switch/case

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -16,14 +16,24 @@
 @endphp
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[1]['nome'] }}
+    Fornecedor: {{ $fornecedores[2]['nome'] }}
     <br>
-    Status: {{ $fornecedores[1]['status'] }}
+    Status: {{ $fornecedores[2]['status'] }}
     <br>
-    CNPJ: {{ $fornecedores[1]['cnpj'] ?? 'Dado não foi preenchido' }}
-    <!--
-        $variável testada não estiver definida (isset)
-        ou
-        $variável testada possuir o valor null
-    -->
+    CNPJ: {{ $fornecedores[2]['cnpj'] ?? 'Dado não foi preenchido' }}
+    <br>
+    Telefone: ({{ $fornecedores[2]['ddd'] ?? '' }}) {{ $fornecedores[2]['telefone'] ?? '' }}
+    @switch($fornecedores[2]['ddd'])
+        @case ('11')
+            São Paulo - SP
+            @break
+        @case ('32')
+            Juiz de Fora - MG
+            @break
+        @case ('85')
+            Fortaleza - CE
+            @break
+        @default
+            Estado não indentificado
+    @endswitch
 @endisset
