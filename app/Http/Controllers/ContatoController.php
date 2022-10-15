20aded8805fff3aac221c1babda2c53ee77ac6ca
Gustavo Ferreira Delfino <gustavo.delfino@kyndryl.com>
2022-10-15 19:53:27 -0300

    134. Customizando as mensagens de feedback de validação

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -40,13 +40,28 @@ class ContatoController extends Controller
 
     public function salvar(Request $request) 
     {
-        $request->validate([
+        $regras = [
             'nome' => 'required|min:3|max:40|unique:site_contatos',
             'telefone' => 'required',
             'email' => 'email',
             'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000'
-        ]); // validate metodo que valida os inputs dos campos
+        ];
+
+        $feedback = [
+            'nome.min' => 'O campo nome precisa ter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome deve ter no maximo 40 caracteres',
+            'nome.unique' => 'O nome informado já está em uso',
+
+            'email.email' => 'O email informado não é valido',
+
+            'mensagem.max' => 'A mensagem deve ter no máximo 2000 caracteres',
+
+            'required' => 'O campo :attribute deve ser preenchido'
+        ];
+
+        $request->validate($regras, $feedback); // validate metodo que valida os inputs dos campos
+
         SiteContato::create($request->all()); // create segue a mesma base do metodo fill(), porem não é preciso do metodo save()
         return redirect()->route('site.index');
     }
