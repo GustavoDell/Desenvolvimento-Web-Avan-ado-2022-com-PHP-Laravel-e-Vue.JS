c79fbea6039ee64dfb81e51012b890cec00af990
Gustavo Ferreira Delfino <gustavo.delfino@kyndryl.com>
2022-11-02 17:30:01 -0300

    144. Manipulando a resposta de uma requisição via middleware

--- app/Http/Middleware/LogAcessoMiddleware.php
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -21,7 +21,12 @@ class LogAcessoMiddleware
         $rota = $request->getRequestUri();
         LogAcesso::create(['log' => "IP $ip requisito a rota $rota"]);
 
-        return $next($request);
+        //return $next($request);
 
+        $resposta = $next($request);
+
+        $resposta->setStatusCode(201, 'O status da resposta e o texto da resposta foram modificados!!!');
+
+        return $resposta;
     }
 }
