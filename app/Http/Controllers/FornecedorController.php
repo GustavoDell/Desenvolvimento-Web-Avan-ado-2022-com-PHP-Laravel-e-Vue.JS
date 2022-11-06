a920dfefcc1c4ffc071d8b78d2fe95633bb3e127
Gustavo Ferreira Delfino <gustavo.delfino@kyndryl.com>
2022-11-06 17:27:25 -0300

    155. Implementando o cadastro de fornecedores parte 4 (atualização de registros)

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -25,8 +25,9 @@ class FornecedorController extends Controller
     public function adicionar(Request $request) {
 
         $msg = '';
-
-        if($request->input('_token') != '') {
+        
+        //inclusão
+        if($request->input('_token') != '' && $request->input('_token') == '') {
             //validacao
             $regras = [
                 'nome' => 'required|min:3|max:40',
@@ -52,6 +53,26 @@ class FornecedorController extends Controller
             $msg = 'Cadastro realizado com sucesso';
         }
 
+        //edição
+        if($request->input('_token') != '' && $request->input('_token') != '') {
+            $fornecedor = Fornecedor::find($request->input('id'));
+            $update = $fornecedor->update($request->all());
+
+            if($update) {
+                $msg = 'Atualização realizado com sucesso';
+            } else {
+                $msg = 'Erro ao tentar atualizar o registro';
+            }
+
+            return redirect()->route('app.fornecedor.editar', ['id' => $request->input('id'), 'msg' => $msg]);
+        }
         return view('app.fornecedor.adicionar', ['msg' => $msg]);
+    }
+
+    public function editar($id, $msg = '') {
+
+        $fornecedor = Fornecedor::find($id);
+
+        return view('app.fornecedor.adicionar', ['fornecedor' => $fornecedor, 'msg' => $msg]);
     }
 }
