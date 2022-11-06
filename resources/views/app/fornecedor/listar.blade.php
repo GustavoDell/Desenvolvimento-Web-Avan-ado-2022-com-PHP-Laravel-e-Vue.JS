aea17f619661457f2ae31cf29e466a9c2d9d1ee7
Gustavo Ferreira Delfino <gustavo.delfino@kyndryl.com>
2022-11-06 17:59:47 -0300

    156. Paginação de registros

--- resources/views/app/fornecedor/listar.blade.php
+++ resources/views/app/fornecedor/listar.blade.php
@@ -40,6 +40,8 @@
                         @endforeach
                     </tbody>
                </table>
+
+               {{ $fornecedores->appends($request)->links() }}
             </div>
         </div>
     </div>
