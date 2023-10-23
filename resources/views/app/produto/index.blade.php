30916c0364c4b311618cf88c17ba3f9d05eeec78
Gustavo Ferreira Delfino <Gustavo.Delfino@kyndryl.com>
2023-10-23 20:41:31 -0300

    197. Eloquent ORM N para N - Praticando um pouco mais o relacionamento belongsToMany

--- resources/views/app/produto/index.blade.php
+++ resources/views/app/produto/index.blade.php
@@ -31,7 +31,6 @@
                             <th>Largura</th>
                             <th></th>
                             <th></th>
-                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -56,6 +55,17 @@
                                     </form>
                                 </td>
                                 <td><a href="{{ route('produto.edit', ['produto' => $produto->id]) }}">Editar</a></td>
+                            </tr>
+
+                            <tr>
+                                <td colspan="12">
+                                    <p>Pedidos</p>
+                                    @foreach ($produto->pedidos as $pedido)
+                                        <a href="{{ route('pedido-produto.create', ['pedido' => $pedido->id]) }}">
+                                            Pedido: {{ $pedido->id }},
+                                        </a>
+                                    @endforeach
+                                </td>
                             </tr>
                         @endforeach
                     </tbody>
